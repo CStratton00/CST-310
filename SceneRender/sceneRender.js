2b1a96b04d90db112fe9e8ec75240ba4225eeb11
Collin Stratton <Collin.Stratton00@gmail.com>
2021-10-09 16:19:33 -0700

    created individual colors for objects

--- SceneRender/sceneRender.js
+++ SceneRender/sceneRender.js
@@ -14,9 +14,10 @@ const cubeVerts = [
     vec4(  0.5, -0.5, -0.5, 1.0 )
 ];
 
-function colors(color) {
-    var red = [
-        [ 0.71, 0.4, 0.21, 1.0 ],   // arm color
+function colorSelect(color) {
+
+    var orange = [
+        [ 0.71, 0.4, 0.21, 1.0 ],   // color
         [ 1.0, 0.65, 0.43, 1.0 ],   // front
         [ 0.71, 0.4, 0.21, 1.0 ],   // right
         [ 0.0, 0.0, 0.0, 1.0 ],     // bottom
@@ -25,7 +26,53 @@ function colors(color) {
         [ 1.0, 0.7, 0.5, 1.0 ],     // top
         [ 1.0, 1.0, 1.0, 1.0 ]      // white
     ];
-    
+
+    var brown = [
+        [ 0.376, 0.2, 0.062, 1.0 ],   // color
+        [ 0.819, 0.560, 0.360, 1.0 ],   // front
+        [ 0.376, 0.2, 0.062, 1.0 ],   // right
+        [ 0.0, 0.0, 0.0, 1.0 ],     // bottom
+        [ 0.63, 0.32, 0.17, 1.0 ],  // back
+        [ 0.376, 0.2, 0.062, 1.0 ],   // left
+        [ 1.0, 0.7, 0.5, 1.0 ],     // top
+        [ 1.0, 1.0, 1.0, 1.0 ]      // white
+    ];
+
+    var black = [
+        [ 0.71, 0.4, 0.21, 1.0 ],   // color
+        [ 1.0, 0.65, 0.9, 1.0 ],   // front
+        [ 0.71, 0.4, 0.9, 1.0 ],   // right
+        [ 0.0, 0.0, 0.9, 1.0 ],     // bottom
+        [ 0.63, 0.32, 0.17, 1.0 ],  // back
+        [ 0.9, 0.55, 0.38, 1.0 ],   // left
+        [ 1.0, 0.7, 0.5, 1.0 ],     // top
+        [ 1.0, 1.0, 1.0, 1.0 ]      // white
+    ];
+
+    var room = [
+        [ 0.71, 0.4, 0.21, 1.0 ],   // color
+        [ 0.929, 0.850, 0.792, 1.0 ],   // front
+        [ 0.847, 0.737, 0.650, 1.0 ],   // right
+        [ 0.627, 0.529, 0.450, 1.0 ],     // bottom
+        [ 0.63, 0.32, 0.17, 1.0 ],  // back
+        [ 0.847, 0.737, 0.650, 1.0 ],   // left
+        [ 0.149, 0.203, 0.270, 1.0 ],     // top
+        [ 1.0, 1.0, 1.0, 1.0 ]      // white
+    ];
+
+    switch(color) {
+        case 'orange':
+            return orange
+        case 'brown':
+            return brown
+        case 'black':
+            return black
+        case 'room':
+            return room
+        default:
+            return
+    }
+
 }
 
 
@@ -50,8 +97,8 @@ var eye;
 const at = vec3( 0.0, 0.0, 0.0 );
 const up = vec3( 0.0, 1.0, 0.0 );
 
-window.onload = function init()
-{
+window.onload = function init() {
+
     canvas = document.getElementById( "gl-canvas" );
 
     gl = WebGLUtils.setupWebGL( canvas );
@@ -68,26 +115,26 @@ window.onload = function init()
     var scalar = 0.9;
 
     // walls
-    colorCubeWalls([-1.975, 0, 0.25], [0.05, 2, 1])
-    colorCubeWalls([1.975, 0, 0.25], [0.05, 2, 1])
-    colorCube([0, 0.975, 0.25], [4, 0.05, 1])
-    colorCube([0, -0.975, 0.25], [4, 0.05, 1])
-    colorCubeWalls([0, 0, -0.25], [4, 2, 0.02])
+    colorCubeWalls([-1.975, 0, 0.25], [0.05, 2, 1], 'room')
+    colorCubeWalls([1.975, 0, 0.25], [0.05, 2, 1], 'room')
+    colorCube([0, 0.975, 0.25], [4, 0.05, 1], 'room')
+    colorCube([0, -0.975, 0.25], [4, 0.05, 1], 'room')
+    colorCubeWalls([0, 0, -0.25], [4, 2, 0.02], 'room')
 
     //
     // chair 1
     //
 
     // arm rests
-    colorCube([0.04 + chair1XAdj, 0 + chair1YAdj, 0], [0.2 * scalar, 0.9 * scalar ,0.7 * scalar]);
-    colorCube([0.76 + chair1XAdj, 0 + chair1YAdj, 0], [0.2 * scalar, 0.9 * scalar, 0.7 * scalar]);
+    colorCube([0.04 + chair1XAdj, 0 + chair1YAdj, 0], [0.2 * scalar, 0.9 * scalar ,0.7 * scalar], 'orange');
+    colorCube([0.76 + chair1XAdj, 0 + chair1YAdj, 0], [0.2 * scalar, 0.9 * scalar, 0.7 * scalar], 'orange');
 
     // bottom cushions
-    colorCube([0.4 + chair1XAdj, -0.16 + chair1YAdj, 0], [0.6 * scalar, 0.2 * scalar, 0.7 * scalar]);
-    colorCube([0.4 + chair1XAdj, 0 + chair1YAdj, 0], [0.6 * scalar, 0.2 * scalar, 0.7 * scalar]);
+    colorCube([0.4 + chair1XAdj, -0.16 + chair1YAdj, 0], [0.6 * scalar, 0.2 * scalar, 0.7 * scalar], 'orange');
+    colorCube([0.4 + chair1XAdj, 0 + chair1YAdj, 0], [0.6 * scalar, 0.2 * scalar, 0.7 * scalar], 'orange');
 
     // back rest
-    colorCube([0.4 + chair1XAdj, 0.25 + chair1YAdj, -0.225], [0.6 * scalar, 0.5 * scalar, 0.2 * scalar]);
+    colorCube([0.4 + chair1XAdj, 0.25 + chair1YAdj, -0.225], [0.6 * scalar, 0.5 * scalar, 0.2 * scalar], 'orange');
 
 
     //
@@ -95,34 +142,34 @@ window.onload = function init()
     //
 
     // arm rests
-    colorCube([0.04 + chair2XAdj, 0 + chair2YAdj, 0], [0.2 * scalar, 0.9 * scalar, 0.7 * scalar]);
-    colorCube([0.76 + chair2XAdj, 0 + chair2YAdj, 0], [0.2 * scalar, 0.9 * scalar, 0.7 * scalar]);
+    colorCube([0.04 + chair2XAdj, 0 + chair2YAdj, 0], [0.2 * scalar, 0.9 * scalar, 0.7 * scalar], 'orange');
+    colorCube([0.76 + chair2XAdj, 0 + chair2YAdj, 0], [0.2 * scalar, 0.9 * scalar, 0.7 * scalar], 'orange');
 
     // bottom cushions
-    colorCube([0.4 + chair2XAdj, -0.16 + chair2YAdj, 0], [0.6 * scalar, 0.2 * scalar, 0.7 * scalar]);
-    colorCube([0.4 + chair2XAdj, 0 + chair2YAdj, 0], [0.6 * scalar, 0.2 * scalar, 0.7 * scalar]);
+    colorCube([0.4 + chair2XAdj, -0.16 + chair2YAdj, 0], [0.6 * scalar, 0.2 * scalar, 0.7 * scalar], 'orange');
+    colorCube([0.4 + chair2XAdj, 0 + chair2YAdj, 0], [0.6 * scalar, 0.2 * scalar, 0.7 * scalar], 'orange');
 
     // back rest
-    colorCube([0.4 + chair2XAdj, 0.25 + chair2YAdj, -0.225], [0.6 * scalar, 0.5 * scalar, 0.2 * scalar]);
+    colorCube([0.4 + chair2XAdj, 0.25 + chair2YAdj, -0.225], [0.6 * scalar, 0.5 * scalar, 0.2 * scalar], 'orange');
 
     //
     // Bookshelf
     //
 
     //back
-    colorCube([0 + bookshelfXAdj,0+ bookshelfYAdj,0],     [0.7,1.5,0.1]);
+    colorCube([0 + bookshelfXAdj,0+ bookshelfYAdj,0], [0.7,1.5,0.1], 'brown');
 
     // shelves
-    colorCube([0 + bookshelfXAdj,0.75+ bookshelfYAdj,0.2], [0.7,0.05,0.5]);
-    colorCube([0 + bookshelfXAdj,0.45+ bookshelfYAdj,0.2], [0.7,0.05,0.5]);
-    colorCube([0 + bookshelfXAdj,0.15+ bookshelfYAdj,0.2], [0.7,0.05,0.5]);
-    colorCube([0 + bookshelfXAdj,-0.15+ bookshelfYAdj,0.2], [0.7,0.05,0.5]);
-    colorCube([0 + bookshelfXAdj,-0.45+ bookshelfYAdj,0.2], [0.7,0.05,0.5]);
-    colorCube([0 + bookshelfXAdj,-0.75+ bookshelfYAdj,0.2], [0.7,0.05,0.5]);
+    colorCube([0 + bookshelfXAdj,0.75+ bookshelfYAdj,0.2], [0.7,0.05,0.5], 'brown');
+    colorCube([0 + bookshelfXAdj,0.45+ bookshelfYAdj,0.2], [0.7,0.05,0.5], 'brown');
+    colorCube([0 + bookshelfXAdj,0.15+ bookshelfYAdj,0.2], [0.7,0.05,0.5], 'brown');
+    colorCube([0 + bookshelfXAdj,-0.15+ bookshelfYAdj,0.2], [0.7,0.05,0.5], 'brown');
+    colorCube([0 + bookshelfXAdj,-0.45+ bookshelfYAdj,0.2], [0.7,0.05,0.5], 'brown');
+    colorCube([0 + bookshelfXAdj,-0.75+ bookshelfYAdj,0.2], [0.7,0.05,0.5], 'brown');
 
     //sides
-    colorCube([-0.35 + bookshelfXAdj,-0.025+ bookshelfYAdj,0.2], [0.05,1.6,0.5]);
-    colorCube([0.35 + bookshelfXAdj,-0.025+ bookshelfYAdj,0.2], [0.05,1.6,0.5]);
+    colorCube([-0.35 + bookshelfXAdj,-0.025+ bookshelfYAdj,0.2], [0.05,1.6,0.5], 'brown');
+    colorCube([0.35 + bookshelfXAdj,-0.025+ bookshelfYAdj,0.2], [0.05,1.6,0.5], 'brown');
 
     gl.viewport( 0, 0, canvas.width, canvas.height );
     gl.clearColor( 1.0, 1.0, 1.0, 1.0 );
@@ -171,29 +218,29 @@ window.onload = function init()
     render();
 }
 
-function colorCube(pos, scale)
-{
-    quad( 1, 0, 3, 2, pos, scale); //front
-    quad( 2, 3, 7, 6, pos, scale); //right side
-    quad( 3, 0, 4, 7, pos, scale); //bottom
-    quad( 6, 5, 1, 2, pos, scale); //top
-    quad( 4, 5, 6, 7, pos, scale); //back
-    quad( 5, 4, 0, 1, pos, scale); //left
+function colorCube(pos, scale, color) {
+
+    quad( 1, 0, 3, 2, pos, scale, color); //front
+    quad( 2, 3, 7, 6, pos, scale, color); //right side
+    quad( 3, 0, 4, 7, pos, scale, color); //bottom
+    quad( 6, 5, 1, 2, pos, scale, color); //top
+    quad( 4, 5, 6, 7, pos, scale, color); //back
+    quad( 5, 4, 0, 1, pos, scale, color); //left
+
 }
 
-function colorCubeWalls(pos, scale)
-{
-    quad( 1, 0, 3, 2, pos, scale); //front
-    quad( 2, 3, 7, 6, pos, scale); //right side
-    quad( 3, 0, 4, 7, pos, scale); //bottom
-    quad( 6, 5, 1, 2, pos, scale); //top
-    quad( 4, 5, 6, 7, pos, scale); //back
-    quad( 5, 4, 0, 1, pos, scale); //left
+function colorCubeWalls(pos, scale, color) {
+
+    quad( 1, 0, 3, 2, pos, scale, color); //front
+    quad( 2, 3, 7, 6, pos, scale, color); //right side
+    quad( 3, 0, 4, 7, pos, scale, color); //bottom
+    quad( 6, 5, 1, 2, pos, scale, color); //top
+    quad( 4, 5, 6, 7, pos, scale, color); //back
+    quad( 5, 4, 0, 1, pos, scale, color); //left
+
 }
 
-function quad(a, b, c, d, pos, scale)
-{
-
+function quad(a, b, c, d, pos, scale, color) {
 
     // We need to parition the quad into two triangles in order for
     // WebGL to be able to render it.  In this case, we create two
@@ -217,16 +264,15 @@ function quad(a, b, c, d, pos, scale)
 
         points.push( currPoint );
 
-        //colors.push( vertexColors[indices[i]] );
+        // colors.push( vertexColors[indices[i]] );
 
         // for solid colored faces use
-        colors.push(vertexColors[a]);
+        colors.push(colorSelect(color)[a]);
 
     }
 }
 
-function render()
-{
+function render() {
 
     gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
@@ -239,7 +285,7 @@ function render()
             eye = vec3(radius * Math.sin(theta - 0.4) * Math.cos(phi), radius * Math.sin(theta - 0.4) * Math.sin(phi), radius * Math.cos(theta - 0.4));
             break;
         case 3:
-            eye = vec3(radius * Math.sin(theta + 0.5) * Math.cos(phi + 0.6), radius * Math.sin(theta + 5.5) * Math.sin(phi + 0.6), radius * Math.cos(theta + 0.5));
+            eye = vec3(radius * Math.sin(theta + 0.5) * Math.cos(phi - 0.6), radius * Math.sin(theta + 5.5) * Math.sin(phi - 0.6), radius * Math.cos(theta + 0.5));
             break;
 
     }
