d4014a686271cbcbef8a01ce12c8490e8907064e
Collin Stratton <Collin.Stratton00@gmail.com>
2021-10-27 14:23:04 -0700

    made the file look better

--- FractalTreeProject/fractaltree.cpp
+++ FractalTreeProject/fractaltree.cpp
@@ -5,24 +5,35 @@
 using namespace std;
 
 string expander(string input, int depth = 1) {
-  if (depth == 0) {
-    return input;
-  } else {
-    int i = 0;
-    while (i < input.length()) {
-      if (input[i] == 'F') {
-        input.insert((i+1), 1, '[');
-        input.insert((i+2), 1, '+');
-        input.insert((i+3), 1, 'F');
-        input.insert((i+4), 1, ']');
-        i += 5;
-      } else {
-        i++;
-      }
+
+    if (depth == 0) {
+
+        return input;
+
+    } else {
+
+        int i = 0;
+        while (i < input.length()) {
+
+            if (input[i] == 'F') {
+
+                input.insert((i+1), 1, '[');
+                input.insert((i+2), 1, '+');
+                input.insert((i+3), 1, 'F');
+                input.insert((i+4), 1, ']');
+
+                i += 5;
+
+            } else {
+
+                i++;
+
+            }
+        }
+
+        depth--;
+        return expander(input, depth);
     }
-    depth--;
-    return expander(input, depth);
-  }
 }
 
 string grammar(string input, float len, float xrot, float yrot, float zrot) {
@@ -35,18 +46,28 @@ string grammar(string input, float len, float xrot, float yrot, float zrot) {
 }
 
 int main() {
-  string input;
-  fstream myFile;
-  myFile.open("file.in");
-  if (!myFile) {
-    cout << "File not found";
-  }
-  // Current file contents for testing F[+F]
-  myFile >> input;
-  myFile.close();
-  string output = expander(input, 10);
-  cout << output;
-  myFile.open("file.out");
-  myFile << output;
-  myFile.close();
+
+    string input;
+    fstream myFile;
+    myFile.open("file.in");
+
+    if (!myFile) {
+        cout << "File not found";
+    }
+
+    // Current file contents for testing F[+F]
+    myFile >> input;
+    myFile.close();
+
+    cout << "Starting recursion\n";
+    string output = "";
+
+    output = expander(input, 5);
+    cout << output << endl;
+    cout << "Recursion concluded\n";
+    
+    myFile.open("file.out");
+    myFile << output;
+    myFile.close();
+
 }
