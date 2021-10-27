3fcd8522a47a4dc8d0d331abae6069895c647b3f
Austin Chandler <59617101+Austin-Chandler@users.noreply.github.com>
2021-10-27 21:14:56 +0000

    added FUNction

--- FractalTreeProject/fractaltree.cpp
+++ FractalTreeProject/fractaltree.cpp
@@ -5,56 +5,48 @@
 using namespace std;
 
 string expander(string input, int depth = 1) {
-
-    if (depth == 0) {
-
-        return input;
-
-    } else {
-
-        int i = 0;
-        while (i < input.length()) {
-
-            if (input[i] == 'F') {
-
-                input.insert((i+1), 1, '[');
-                input.insert((i+2), 1, '+');
-                input.insert((i+3), 1, 'F');
-                input.insert((i+4), 1, ']');
-
-                i += 5;
-
-            } else {
-
-                i++;
-
-            }
-        }
-
-        depth--;
-        return expander(input, depth);
+  if (depth == 0) {
+    return input;
+  } else {
+    int i = 0;
+    while (i < input.length()) {
+      if (input[i] == 'F') {
+        input.insert((i+1), 1, '[');
+        input.insert((i+2), 1, '+');
+        input.insert((i+3), 1, 'F');
+        input.insert((i+4), 1, ']');
+        i += 5;
+      } else {
+        i++;
+      }
     }
+    depth--;
+    return expander(input, depth);
+  }
+}
+
+string grammar(string input, float len, float xrot, float yrot, float zrot) {
+  /*
+
+    Grammar rules:
+
+
+  */
 }
 
 int main() {
-
-    string input;
-    fstream myFile;
-    myFile.open("file.in");
-
-    if (!myFile) {
-        cout << "File not found";
-    }
-
-    // Current file contents for testing F[+F]
-    myFile >> input;
-    myFile.close();
-
-    cout << "Starting recursion\n";
-    string output = "";
-
-    output = expander(input, 5);
-    cout << output << endl;
-    cout << "Recursion concluded\n";
-
+  string input;
+  fstream myFile;
+  myFile.open("file.in");
+  if (!myFile) {
+    cout << "File not found";
+  }
+  // Current file contents for testing F[+F]
+  myFile >> input;
+  myFile.close();
+  string output = expander(input, 10);
+  cout << output;
+  myFile.open("file.out");
+  myFile << output;
+  myFile.close();
 }
